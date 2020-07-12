c474796d49f1ef21582504ae43b2ef764eb44d71
Mohd Faisal <mohd@thetrustsearch.com>
2020-07-12 21:36:15 +0530

    add form-hook to reuse stateful logic

--- src/components/AddCandidate/index.js
+++ src/components/AddCandidate/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
 import Input from '../shared/FormElements/Input';
+import { useForm } from '../hooks/form-hook';
 import { VALIDATOR_REQUIRE } from '../../utils/validators';
 
 const AddCandidate = () => {
+	const [formState, inputHandler] = useForm(
+		{
+			name: {
+				value: '',
+				isValid: false,
+			},
+			email: {
+				value: '',
+				isValid: false,
+			},
+			phone: {
+				value: '',
+				isValid: false,
+			},
+			department: {
+				value: '',
+				isValid: false,
+			},
+		},
+		false
+	);
 	return (
 		<div>
 			<Input
@@ -14,6 +36,8 @@ const AddCandidate = () => {
 				label="Candidate's Name *"
 				errorText='Required'
 				validators={[VALIDATOR_REQUIRE()]}
+				onInput={inputHandler}
+				fullWidth={true}
 			/>
 		</div>
 	);
--- src/components/shared/FormElements/Input/index.js
+++ src/components/shared/FormElements/Input/index.js
@@ -32,9 +32,9 @@ const Input = (props) => {
 	const { id, onInput } = props;
 	const { value, isValid } = inputState;
 
-	// useEffect(() => {
-	// 	onInput(id, value, isValid);
-	// }, [id, value, isValid, onInput]);
+	useEffect(() => {
+		onInput(id, value, isValid);
+	}, [id, value, isValid, onInput]);
 
 	const changeHandler = (event) => {
 		dispatch({
@@ -61,7 +61,6 @@ const Input = (props) => {
 					variant,
 					label,
 					disabled,
-					error,
 					errorText,
 					multiline,
 					rows,
@@ -78,8 +77,8 @@ const Input = (props) => {
 						label={label}
 						value={inputState.value}
 						disabled={disabled}
-						error={error}
-						helperText={error && errorText}
+						error={!inputState.isValid && inputState.isTouched}
+						helperText={!inputState.isValid && inputState.isTouched && errorText}
 						multiline={multiline}
 						rows={rows}
 						InputProps={{ ...InputProps }}
