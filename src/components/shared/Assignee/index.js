9b48187a637f448017b7b1d89507cf381e5d6c6f
Mohd Faisal <mohd@thetrustsearch.com>
2020-07-13 14:38:29 +0530

    add assignee logic

--- src/components/shared/Assignee/index.js
+++ src/components/shared/Assignee/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { useSelector, shallowEqual } from 'react-redux';
 import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
 
+import { HR } from '../../../utils/departmentConstants';
 import styles from './style.module.css';
 
 const Assignee = () => {
+	const assignee = useSelector((state) => {
+		return state?.onBoardingInfo?.assignee ?? HR;
+	}, shallowEqual);
 	return (
 		<div className={styles.assigneeContent}>
 			<div className={`${styles.assigneeContentItem} ${styles.assigneeIcon}`}>
 				<AssignmentIndIcon />
 			</div>
-			<div className={styles.assigneeContentItem}>HR (Assignee)</div>
+			<div className={styles.assigneeContentItem}>{assignee.toUpperCase()} (Assignee)</div>
 		</div>
 	);
 };
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,4 +1,15 @@
+import { departmentConstantsArray, HR } from '../../utils/departmentConstants';
+
 export const saveData = (type, candidateData, step) => {
-	//assignee
-	return { type, payload: { candidateInfo: { ...candidateData }, step } };
+	let assignee = HR;
+	if (step === 2) {
+		const { department } = candidateData;
+		const deptIndex = departmentConstantsArray.findIndex((dept) => {
+			return dept?.value === department;
+		});
+		if (deptIndex > -1) {
+			assignee = departmentConstantsArray[deptIndex]?.value ?? HR;
+		}
+	}
+	return { type, payload: { candidateInfo: { ...candidateData }, step, assignee } };
 };
